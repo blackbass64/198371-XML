bd27e0b5abe345b794a5f4b1173e731974d66982
Athibet Prawane <athibet.pi@gmail.com>
2018-11-15 03:04:09 +0700

    commit lectures

--- node/exam.js
+++
@@ -1,9 +0,0 @@
-var request1 = require("request");
-request1.get("http://httpbin.org/ip", (error, response, body) => {
-    if (error) {
-        return console.dir(error);
-    }
-    var result = JSON.parse(body);
-    var ip = result.origin;
-    console.dir(ip);
-})
