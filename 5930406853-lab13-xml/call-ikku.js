10f830685e48c601b896e348d75a4101f4ee2b11
Athibet Prawane <athibet.pi@gmail.com>
2018-11-15 04:05:03 +0700

    commit lab13

---
+++ 5930406853-lab13-xml/call-ikku.js
@@ -0,0 +1,36 @@
+var request1 = require("request");
+var http = require("http");
+
+request1.get("https://www.kku.ac.th/ikku/api/activities/services/topActivity.php", (error, response, body) => {
+    if (error) {
+        return console.dir(error);
+    }
+    var activityOBJ = JSON.parse(body);
+    
+
+    var act = activityOBJ.activities;
+    http.createServer(function (req, res) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8;'
+        });
+
+        res.write("<h2 style=\"margin-left: 20px;\">Top Activities in Khon Kaen University</h2>");
+        res.write("<hr>");
+        
+        res.write("<table>");
+        res.write("<tbody>");
+        
+        for (var i = 0; i < act.length; i++) {
+            res.write("<tr>");
+            res.write("<td style=\"padding-left: 20px;\">" + (i+1) + ". </td>");
+            res.write("<td style=\"padding-left: 20px;\">" + act[i].dateSt + "</td>");
+            res.write("<td style=\"padding-left: 20px;\"><a href=\"" + act[i].url + "\">" + act[i].title + "</a></td>");
+            res.write("<td style=\"padding-left: 20px;\">" + act[i].contact.phone + "</td>");
+            res.write("<td style=\"padding-left: 20px;\">");
+        }
+        res.write("</table>");
+        res.write("</tbody>");
+        
+    }).listen(8080, '127.0.0.1');
+    console.log('Server running at http://127.0.0.1:8080')
+});
